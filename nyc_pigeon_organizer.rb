b5c27c850526e55edd5ca56cb2ded56d027b143a
Hector Polanco <hdp2112@columbia.edu>
2019-10-24 04:57:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  data.each do |key, value|
+    value.each do |subkey, name|
+      name.each do |name|
+        if !new_data[name]
+          new_data[name] = {}
+        end
+        
+        if !new_data[name][key]
+          new_data[name][key] = []
+        end
+        
+        new_data[name][key] << subkey.to_s
+      end
+    end
+  end
+  new_data
 end
